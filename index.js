24ef465711a385da4d5372db77e811a52b48a17b
sebug <sebastian.gfeller@gmail.com>
2018-12-23 11:11:44 +0100

    Find matching template

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 const fs = require('fs');
 
-console.log(getTemplate());
+const template = getTemplate();
+const startDate = getStartDate(template);
+const firstPeriod = getMatchingPeriod(template, startDate);
+console.log(firstPeriod);
+
+function getMatchingPeriod(template, sd) {
+    for (let period of template) {
+	if (valueOfStructured(period.from) <= valueOfStructured(sd)) {
+	    if (!period.to) {
+		// Open-ended period to the future
+		return period;
+	    }
+	    if (valueOfStructured(sd) <= valueOfStructured(period.to)) {
+		return period;
+	    }
+	}
+    }
+    throw new Error('No period found for date ' + JSON.stringify(sd));
+}
+
+function advanceToWeekDay(date, weekDay) {
+    if (date.getDay() === weekDay) {
+	return date;
+    }
+    let tomorrow = new Date(date.valueOf());
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    return advanceToWeekDay(tomorrow, weekDay);
+}
+
+function valueOfStructured(sd) {
+    return sd.year * 10000 + sd.month * 100 + sd.day;
+}
+
+// the global idea is to use JS dates as little as possible,
+// so we use the structured format { year, month, day }
+function structuredFromDate(d) {
+    return {
+	year: d.getFullYear(),
+	month: d.getMonth() + 1,
+	day: d.getDate()
+    };
+}
+
+function dateFromStructured(d) {
+    return new Date(d.year, d.month - 1, d.day);
+}
+
+function getStartDate(template) {
+    const fromDates = template.map(t => advanceToWeekDay(dateFromStructured(t.from), t.weekDay)).map(structuredFromDate);
+    let min = fromDates[0];
+    for (let date of fromDates) {
+	if (valueOfStructured(date) < valueOfStructured(min)) {
+	    min = date;
+	}
+    }
+    return min;
+}
 
 function getTemplate() {
     const content = fs.readFileSync('template.json','utf-8');
